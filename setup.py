49c3baf2159c49e4131b02f844d7b53eeef81d6f
Mikhail Yu Petrov <mikhail.petrov@corp.mail.ru>
2018-04-25 17:24:27 +0300

    ADD setup.py for a convenient way to install a module.

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(name='icq',
+      version='0.0.8',
+      description='Api library for icq',
+      packages=find_packages(exclude=["example"]),
+      url='https://github.com/icq-bot/python-icq-bot',
+      zip_safe=False)
